f12065d177728d67aa320164449f4db2ecc1fba2
Judge1234 <39850866+Judge1234@users.noreply.github.com>
2019-01-12 22:04:35 -0500

    Update setup.py

--- cfactorial/setup.py
+++ cfactorial/setup.py
@@ -1,8 +1,10 @@
 from distutils.core import setup, Extension
- 
+
+
 module = Extension('cfactorial', sources = ['cfactorial.c'])
  
-setup (name = 'C Factorial',
-        version = 'BETA',
-        description = 'C Factorial Function',
-        ext_modules = [module])
+
+setup(name = 'C Factorial',
+      version = 'BETA',
+      description = 'C Factorial Function',
+      ext_modules = [module])
